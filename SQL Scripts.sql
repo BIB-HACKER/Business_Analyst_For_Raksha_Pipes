d7d3bea1a3293c181ca8cc08a8f5ae91310eb0fb
Bibhakar Paul <87580111+BIB-HACKER@users.noreply.github.com>
2024-05-17 20:21:40 +0530

    Add files via upload

---
+++ SQL Scripts.sql
@@ -0,0 +1,14 @@
+create database raksha_Pipes;
+
+use raksha_pipes;
+
+select * from products;
+select * from customers;
+select * from Sales;
+select * from Suppliers;
+select * from Orders;
+
+drop database raksha_Pipes;
+
+drop table customers;
+
